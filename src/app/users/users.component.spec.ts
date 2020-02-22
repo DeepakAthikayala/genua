a05f7d33ed3121b5854d0422d6efdd531984f6dc
DeepakAthikayala <athikayaladeepak@gmail.com>
2020-02-22 11:44:30 +0100

    test written only in users component

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -3,14 +3,11 @@ import { UsersComponent } from './users.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserService } from '../user.service';
-import { User } from '../user';
-
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
   let userService: UserService;
-
   
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,34 +17,20 @@ describe('UsersComponent', () => {
     }).compileComponents();
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.debugElement.componentInstance;
-    // userService = TestBed.get(UserService);
-    // spyOn(userService, 'getUsers').and.callThrough();
     }));
 
-  
   it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should do something', ()=> {
-
-  //   component.ngOnInit();
-  //   fixture.detectChanges();
-
-  //   expect(userService.getUsers).toHaveBeenCalled();
-  // })
-
   it('should run the test correctly god....', () => {
     let dataService = fixture.debugElement.injector.get(UserService);
     let spy = spyOn(dataService, 'getUsers').length;
-
     fixture.detectChanges();
     fixture.whenStable().then( () => {
       expect(component.users.length).toBeGreaterThanOrEqual(spy);
     })
 
   })
-
-
 
 });
